6f3304fd51f608872a1adb9152e047783876b280
tim760255458 <760255458@qq.com>
2018-05-02 09:36:13 +0800

    周报状态页面完成部分逻辑

--- src/utils/dealWithTime.js
+++ src/utils/dealWithTime.js
@@ -1,3 +1,15 @@
 export default () => {
+  const nowTime = new Date()
+  const day = nowTime.getDay() // 现在是周几
+  const hour = nowTime.getHours() // 现在是几点
+  const minute = nowTime.getMinutes() // 现在是多少分钟
+  const second = nowTime.getSeconds() // 现在是多少秒
 
+  const time = {
+    lave_days: day > 0 ? 7 - day : day,
+    lave_hours: hour > 0 ? 24 - hour : hour,
+    lave_minutes: minute > 0 ? 60 - minute : minute,
+    lave_seconds: second > 0 ? 60 - second : second
+  }
+  return time
 }
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -4,14 +4,24 @@
       <span>本周第{{ nowWeekNumber }}周</span>
       <span>{{ weeklyStatusMessage }}</span>
     </p>
-    <time></time>
+    <time v-if="weeklyStatus === 1">剩余{{ laveTime.lave_days | digitsToDouble }}天{{ laveTime.lave_hours | digitsToDouble }}时{{ laveTime.lave_minutes | digitsToDouble }}分{{ laveTime.lave_seconds | digitsToDouble }}秒</time>
+    <span v-if="weeklyStatus === 2">周报已提交</span>
+    <span v-if="weeklyStatus === 3">本周已请假</span>
+    <span>{{ hint }}</span>
     <button @click="toHelloWorld">go hello,world page</button>
+    <div class="btns">
+      <button>撰写周报</button>
+      <button>我的周报</button>
+      <button>申请请假</button>
+      <button>取消请假</button>
+    </div>
   </div>
 </template>
 
 <script>
 import nowWeek from '../../utils/nowWeek'
 import { getWeeklyStatus } from '../../api'
+import dealWithTime from '../../utils/dealWithTime'
 
 export default {
   name: 'home',
@@ -19,7 +29,18 @@ export default {
     return {
       message: 'this is homepage!',
       nowWeekNumber: null,
-      weeklyStatus: 1
+      weeklyStatus: 1,
+      laveTime: {
+        lave_days: 0,
+        lave_hours: 0,
+        lave_minutes: 0,
+        lave_seconds: 0
+      }
+    }
+  },
+  filters: {
+    digitsToDouble: (value) => {
+      return value > 9 ? value : '0' + value
     }
   },
   computed: {
@@ -40,12 +61,34 @@ export default {
           break
       }
       return message
+    },
+    hint: function () {
+      let message = ''
+      switch (this.weeklyStatus) {
+        case 1:
+          message = '时间不多了，抓紧提交周报哟！'
+          break
+        case 2:
+          message = '开始为下周计划做准备吧~'
+          break
+        case 3:
+          message = '开始为下周计划做准备吧~'
+          break
+        default:
+          message = '时间不多了，抓紧提交周报哟！'
+          break
+      }
+      return message
     }
   },
   mounted () {
     this.getWeekNumbers()
     this.getWeekly()
+    setInterval(() => {
+      this.setTime()
+    }, 1000)
   },
+  destroyed () {},
   methods: {
     toHelloWorld () {
       this.$router.push({ path: '/helloworld' })
@@ -57,6 +100,9 @@ export default {
       getWeeklyStatus(1).then(res => {
         this.weeklyStatus = res.status
       })
+    },
+    setTime () {
+      this.laveTime = dealWithTime()
     }
   }
 }
