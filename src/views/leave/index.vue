95c6fd79a02bfcfc436a8dc49053eb3d12a0005c
tim760255458 <760255458@qq.com>
2018-05-02 13:43:46 +0800

    请假申请页部分逻辑

--- src/views/leave/index.vue
+++ src/views/leave/index.vue
@@ -5,19 +5,21 @@
       <div>
         <span></span>
         <span></span>
+        <span>请假状态</span>
+        <span></span>
       </div>
       <div>
         <span>请假周数</span>
-        <span>一周</span>
-        <span>二周</span>
-        <span>三周</span>
+        <span @click="selectWeek(1)" class="btn-item" :class="{ 'active': selectWeeks === 1 }">一周</span>
+        <span @click="selectWeek(2)" class="btn-item" :class="{ 'active': selectWeeks === 2 }">二周</span>
+        <span @click="selectWeek(3)" class="btn-item" :class="{ 'active': selectWeeks === 3 }">三周</span>
       </div>
       <div>
         <span>请假理由（必填）</span>
-        <textarea placeholder="请假理由不得少于35个字..."></textarea>
+        <textarea v-model="content" placeholder="请假理由不得少于35个字..."></textarea>
       </div>
     </div>
-    <button>提交请假申请</button>
+    <button @click="submit">提交请假申请</button>
   </div>
 </template>
 
@@ -25,11 +27,31 @@
 export default {
   name: 'Leave',
   data () {
-    return {}
+    return {
+      selectWeeks: 0,
+      content: ''
+    }
+  },
+  methods: {
+    selectWeek (val) {
+      this.selectWeeks = val
+    },
+    submit () {
+
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.btn-item {
+  border: 1px solid rgb(49, 199, 19);
+  padding: 3px 10px;
+  border-radius: 3px;
+  color: rgb(49, 199, 19);
+}
+.active {
+  background: rgb(49, 199, 19);
+  color: #fff;
+}
 </style>
