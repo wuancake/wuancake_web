9641ac6d4d1c051dff377f9958045795a7c6c9af
tim760255458 <qq760255458@gmail.com>
2018-06-21 20:45:19 +0800

    接口地址更改

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_MOCK: false,
-  BASE_API: '"http://13.114.107.36:8008/"'
+  BASE_API: '"http://13.125.249.52:8008/"'
 })
